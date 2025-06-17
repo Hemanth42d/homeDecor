b425b7f9787c039febb1a84a89ea2d8ee8025f71
M V Hemanth <venkatahemanth42d@gmail.com>
2025-06-17 16:34:40 +0530

    Modified links a to Link component and made a dropdown option for navbar categories option

--- Frontend/homeDecor/src/components/ShopPage/LinksBar.jsx
+++ Frontend/homeDecor/src/components/ShopPage/LinksBar.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { RiDiscountPercentFill } from "react-icons/ri";
+import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleUp } from "react-icons/fa";
+
+// <FaAngleDown />
+// <FaAngleUp />
 
 const LinksBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <div className="h-15 flex flex-col justify-start items-center border-b-1 bg-zinc-100">
@@ -17,7 +25,35 @@ const LinksBar = () => {
           </p>
           <p className="w-full flex justify-around items-center">
             <Link to="/shop">Shop</Link>
-            <Link to="">Categories</Link>
+            <Link
+              to=""
+              className="flex justify-center items-center gap-1 relative"
+              onClick={() => setIsOpen((prev) => !prev)}
+            >
+              <span>Categories</span>
+              {!isOpen ? <FaAngleDown /> : <FaAngleUp />}
+              {isOpen && (
+                <span className="absolute top-8 w-[12vw] bg-white shadow-lg rounded-md">
+                  <li className="list-none w-full">
+                    <Link to="" className="text-[16px] w-full block p-2">
+                      Home Decoration
+                    </Link>
+                    <Link to="" className="text-[16px] w-full block p-2">
+                      Light Decor
+                    </Link>
+                    <Link to="" className="text-[16px] w-full block p-2">
+                      Kitchen and Dining
+                    </Link>
+                    <Link to="" className="text-[16px] w-full block p-2">
+                      Home Furniture
+                    </Link>
+                    <Link to="" className="text-[16px] w-full block p-2">
+                      Game and Outdoor
+                    </Link>
+                  </li>
+                </span>
+              )}
+            </Link>
           </p>
           <p className="w-full flex justify-center items-center">
             <Link
